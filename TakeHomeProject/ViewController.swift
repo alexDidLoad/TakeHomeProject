c233bde0fb53f89d20f34e21c90b595e12214a63
Alexander Ha <Ha.alex.h@gmail.com>
2021-01-25 18:32:03 -0800

    Initial commit

---
+++ TakeHomeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeHomeProject
+//
+//  Created by Alexander Ha on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
